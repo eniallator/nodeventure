3e90fe2aae291693735ef7e300e9ffa6999b15cd
riccardo.tonini <riccardotonini@gmail.com>
2013-09-12 21:10:29 +0100

    Adding a new object

--- world/cube.js
+++ world/cube.js
@@ -8,8 +8,8 @@ room('spinning_cube', {
   image: 'http://cimota.com/blog/wp-content/uploads/2012/06/22-Cans-Curiosity-490x245.jpg'
 });
 
-item('secretLab', 'jetpack', {
+item('spinning_cube', 'black_cube_fragment', {
   respawnTime: 120,
-  short: 'a jetpack',
-  description: 'An awesome jetpack for flying around.'
+  short: 'A fragment of the ancient black cube...',
+  description: 'The ancient black cube is said to endow its owner with misterious supernatural powers...'
 });
