4f5db5b092ed1b46128ce25a8920b9516f76f076
nseymoursmith <nic.rss@gmail.com>
2013-09-12 20:59:49 +0100

    Update quantum_entrance.js

--- world/quantum_entrance.js
+++ world/quantum_entrance.js
@@ -1,13 +1,7 @@
-room('quantum_entrance', {
-  description: "Welcome to the quantum reality interference experi^H^H^H^H^H^H totally-safe and thoroughly tested transportation room.\n A rotund gentleman dressed in a red overcoat and riding slacks ushers you towards the south exit, ominously signposted 'The quantum interferometric non-local reality probability redistributor'. Dare you enter?",
+ room('quantum_entrance', {
+  description: "Welcome to the quantum reality interference experi^H^H^H^H^H^H totally-safe and thoroughly tested transportation room.\n A rotund gentleman dressed in a red overcoat and riding slacks ushers you towards a large lever, ominously labelled 'The quantum interferometric non-local reality probability redistributor'",
   exits: {
-    north: 'theskiff',
-<<<<<<< HEAD
-    south: 'quantum_teleporter',
-=======
-    //south: 'quantum_teleporter'
->>>>>>> a1cbf961cc73db6f6b10be64a5c9a626ac4556fd
+    north: 'theskiff'
   },
   image: 'myroom.jpg'
 });
-
