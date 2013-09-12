1ea91bd022d203c36989a02a24550cf80db0c2a0
riccardo.tonini <riccardotonini@gmail.com>
2013-09-12 20:54:33 +0100

    Adding new version of cube room

--- world/cube.js
+++ world/cube.js
@@ -7,3 +7,9 @@ room('spinning_cube', {
   },
   image: 'http://cimota.com/blog/wp-content/uploads/2012/06/22-Cans-Curiosity-490x245.jpg'
 });
+
+item('secretLab', 'jetpack', {
+  respawnTime: 120,
+  short: 'a jetpack',
+  description: 'An awesome jetpack for flying around.'
+});
