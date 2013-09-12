c72994351f76496b579ba08eb9bed68d198c9c30
methodik <info@methodik.co.uk>
2013-09-12 20:50:30 +0100

    Created Methodiks Room

---
+++ world/myroom.js
@@ -0,0 +1,8 @@
+handler('enterRoom:home', function (player, room, game) {
+  player.display.eval(function () {
+    // This function will be executed on the client
+
+    $('<h1>Hello world</h1>').appendTo('body');
+    
+  });
+});
