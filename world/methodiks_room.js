0c0bbaf66ff1a22765fe4d83c05cd072e839d4ca
methodik <info@methodik.co.uk>
2013-09-12 21:24:49 +0100

    3 circles now

--- world/methodiks_room.js
+++ world/methodiks_room.js
@@ -12,19 +12,36 @@ handler('enterRoom:methodiks_room', function (player, room, game) {
   player.display.eval(function () {
     // This function will be executed on the client
 
-    $('<div id="circle1">circle1</div>').appendTo('body');
-    $('#circle1').css({
+    $('<div id="circle1" class="circle"></div>').appendTo('body');
+    $('<div id="circle2" class="circle"></div>').appendTo('body');
+    $('<div id="circle3" class="circle"></div>').appendTo('body');
+    $('.circle').css({
     	background: 'white',
     	height: '250px',
     	width: '250px',
-    	borderRadius: '250px',
-    	position: 'absolute',
-    	bottom: 50,
-    	left: 50
+    	borderRadius: '250px'
     });
     var container = $('#container').appendTo('body');
 
-    $.ajax('http://methodik.co.uk/nodeventure/methodiks-room-markup.html');
+    // styles
+    $('#circle1').css({
+    	position: 'absolute',
+    	bottom: 50,
+    	left: 50
+    })
+    $('#circle2').css({
+    	position: 'absolute',
+    	bottom: 50,
+    	left: 300
+    });
+    $('#circle3').css({
+    	position: 'absolute',
+    	bottom: 50,
+    	left: 600
+    });
+
+
+    
 
 
     
