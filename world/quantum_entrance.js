09a716df4737aafd46a9e17b62edf8e6c33790a3
nseymoursmith <nic.rss@gmail.com>
2013-09-12 20:39:48 +0100

    Update quantum_entrance.js
    removed exit to nowhere.

--- world/quantum_entrance.js
+++ world/quantum_entrance.js
@@ -2,7 +2,7 @@ room('quantum_entrance', {
   description: "Welcome to the quantum reality interference experi^H^H^H^H^H^H totally-safe and thoroughly tested transportation room.\n A rotund gentleman dressed in a red overcoat and riding slacks ushers you towards the south exit, ominously signposted 'The quantum interferometric non-local reality probability redistributor'. Dare you enter?",
   exits: {
     north: 'theskiff',
-    south: 'quantum_teleporter'
+    //south: 'quantum_teleporter'
   },
   image: 'myroom.jpg'
 });
