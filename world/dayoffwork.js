9f5f20e90116fdb9577603c100e6fb97943e9895
dan12321 <daniel.songi10@gmail.com>
2013-09-12 20:40:27 +0100

    Update dayoffwork.js

--- world/dayoffwork.js
+++ world/dayoffwork.js
@@ -1,5 +1,6 @@
 command("CallBoss", "make a phone call", function(rest,player,game){
   player.write("Hello?")
+  
   player.write("Your trying to get a day off work. What symptom do you choose: A.Bunged up nose - Boss doesn't want you sneezing and sniffing in the office.")
 
   
