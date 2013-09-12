8a0147e57d775c47d0568adbaefc597260c51189
Kirk Northrop <kirk@krn.me.uk>
2013-09-12 21:26:35 +0100

    Update dopefish.js

--- world/dopefish.js
+++ world/dopefish.js
@@ -1,12 +1,11 @@
 var dopefish = character('dopefish', {
   location: 'beige',
-  description: 'half man, half monkey,half bear and all yeti. He seems to like the snow.',
-  image: 'http://pressthebuttons.typepad.com/photos/uncategorized/dopefish.png'
+  description: 'half man, half monkey,half bear and all yeti. He seems to like the snow.'
 });
 
 handler('tick', function () {
-  // every 300 seconds on average
-  if (Math.random() * 3 < 1) {
+  // every 30 seconds on average
+  if (Math.random() * 30 < 1) {
     var room = dopefish.getCurrentRoom(),
         exits = _.keys(room.exits),
         i = Math.floor(Math.random()*exits.length),
@@ -16,8 +15,6 @@ handler('tick', function () {
     
     var i = parseInt(Math.random() * roomPlayers.length);
     
-    room.broadcast("debug:" + i);
-    
     roomPlayers[i].write("The dopefish says \"Duh!\"");
     roomPlayers[i].display.show('http://pressthebuttons.typepad.com/photos/uncategorized/dopefish.png');
   }
@@ -25,6 +22,6 @@ handler('tick', function () {
 
 handler('playerTalk', function (player, message) {
   if (player.getCurrentRoom() === dopefish.getCurrentRoom() && dopefish !== player) {
-    dopefish.execute('say WAAAARGH');
+    //dopefish.execute('say WAAAARGH');
   }
 });
