a8230fa166901aaf421783af4d29f722294ac52d
Andy Broomfield <andybroomfield@gmail.com>
2013-09-12 22:31:59 +0100

    Create maroonedguy.js

---
+++ world/maroonedguy.js
@@ -0,0 +1,4 @@
+var maroonedguy = character('maroonedguy', {
+  location: 'island',
+  description: 'A scrawny old man whos been marooned on this island for the last 5 years, he\'s not as wise as he once was.'
+});
