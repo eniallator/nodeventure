737db3f98dcc9b56d9ee7754e25a5a079ede2a3a
Andy Broomfield <andybroomfield@gmail.com>
2013-09-12 21:12:48 +0100

    Add the bottle and boat to the beach

--- world/beach.js
+++ world/beach.js
@@ -2,4 +2,18 @@ room('beach', {
   description: "waves crash upon a stony beach.",
   image: "http://farm9.staticflickr.com/8248/8502899877_19c3a4a6f7.jpg",
   exits: { south: 'sea', north: 'beachpath' },
+  items: [
+    {
+      name: 'bottle',
+      short: 'a bottle',
+      description: 'It looks like there is some sort of paper stuffed inside it.',
+      respawnTimer: 60
+    },
+    {
+      name: 'boat',
+      short: 'a boat',
+      description: 'a fine looking seaworthy craft, if only the sails wernt missing.',
+      respawnTimer: 60
+    },
+  ]
 });
