943b5385cc63165fd0bc719c3cb3af62526df2c8
Kirk Northrop <kirk@krn.me.uk>
2013-09-26 21:36:12 +0100

    Update dollhouse.js

--- world/dollhouse.js
+++ world/dollhouse.js
@@ -1,7 +1,7 @@
 room('dollhouse', {
   description: "This is a murky world, where all sorts of beings hide. Some of them invisible, and others just plain strange!",
   image: "http://cabinetmagazine.org/issues/2/beige.jpg",
-  exits: { east: 'home', south: 'soup kitchen'},
+  exits: { east: 'home', south: 'soupkitchen'},
   items: [
     {
       name: 'threeheads',
