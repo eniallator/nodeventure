00edc177246ce0cc778f5b97a9b2cc6efb54ded8
thHove <tim.hewson@NTLworld.com>
2013-09-26 20:58:56 +0100

    Create hw.js

---
+++ world/hw.js
@@ -0,0 +1,12 @@
+handler('enterRoom', function (player, room, game) {
+    if (room.name === ''){
+      player.display.eval(function(){
+        var body = jQuery('body'),
+            canvas = jQuery('<canvas></canvas>'),
+            ctx = canvas.getContext('2d');
+            
+      
+        
+      });
+    }
+});
