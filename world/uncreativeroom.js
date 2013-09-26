0b6333fe417ac23694cda5b3da9a1d69bb72758c
happytom <ilchenko.daniil280@gmail.com>
2013-09-26 19:59:43 +0100

    Update uncreativeroom.js

--- world/uncreativeroom.js
+++ world/uncreativeroom.js
@@ -1,4 +1,4 @@
-room('The uncreative room', {
+room('the_uncreative_room', {
   description: "A bleak dark room, void of anything withholding or substantial. Being here creates a feeling that imagination and creativity is sapped away moment by moment.",
   image: "http://www.koreanbeacon.com/wp-content/uploads/2011/06/20-lee_ufan_naoshima_museum_silence_room.jpg",
   exits: { south: 'westbeach' },
