6ce23a98e90a45d9cea322340db758fcdeeb270d
ycmjason <ycm.jason@gmail.com>
2013-09-26 20:28:19 +0100

    Update bellerbyscollege.js

--- world/bellerbyscollege.js
+++ world/bellerbyscollege.js
@@ -5,3 +5,19 @@ room('bellerbyscollege', {
   },
   image: 'http://www.bellerbys.com/-/media/Bellerbys%20Site/Images/Locations/Brighton/Brighton_location_campus_220x147.jpg'
 });
+character('lucas', {
+  location: 'bellerbyscollege',
+  description: 'an overpromoted skiver'
+});
+character('franco', {
+  location: 'bellerbyscollege',
+  description: 'a lazy student'
+});
+character('jason', {
+  location: 'bellerbyscollege',
+  description: 'a handsome guy'
+});
+character('gray', {
+  location: 'bellerbyscollege',
+  description: 'an idler'
+});
