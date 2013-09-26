5b36f5721ca9c9b99b4189604cdd925132a60e6a
Andy Baker <andy@andybak.net>
2013-09-26 21:35:54 +0100

    Good god. Are we savages?

--- world/dayoffwork.js
+++ world/dayoffwork.js
@@ -1,6 +1,6 @@
 command("callboss", "make a phone call", function(rest,player,game){
   player.write("Hello?");
-  player.write("Your trying to get a day off work. What symptom do you choose: A.Bunged up nose B.Sore Throat C.Scream in agony");
+  player.write("You're trying to get a day off work. What symptom do you choose: A.Bunged up nose B.Sore Throat C.Scream in agony");
 };
 command("a", function(rest,player,game){
   Player.write("Boss doesn't want you sneazing and sniffing in the office. How do you respond? A. I dont think i can come in today. B.The thought of working has made me sick.");
