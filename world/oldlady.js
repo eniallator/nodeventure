c6ae99f710f6360db6dca8b145558bff22149a16
Alfonso de la Osa <fonz@botverse.com>
2013-09-26 21:46:55 +0100

    oldlady

--- world/oldlady.js
+++ world/oldlady.js
@@ -2,7 +2,8 @@ var oldlady = character('oldlady', {
   location: 'dollhouse',
   description: 'An old lady. She seems strangely interested in one of the dolls in particular.',
   onReceive: function(player, item) {
-    player.write(item);
+
+    player.write(item.name);
   }
 });
 
