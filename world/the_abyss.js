1521a29feb4655c64ed3ddae8738d1c0363059c6
Andy Baker <andy@andybak.net>
2013-09-26 21:02:56 +0100

    nospaces

--- world/the_abyss.js
+++ world/the_abyss.js
@@ -1,4 +1,4 @@
-room('the abyss', {
+room('the_abyss', {
   description: "You are in a fiery abyss. Scorching flames are underfoot. The wailing of damned souls can be heard from all directions but you can see no sign of them through the smoke, ash and flame..",
   image: "http://upload.wikimedia.org/wikipedia/commons/6/65/The_Door_to_Hell.jpg",
   exits: { up: 'home'},
