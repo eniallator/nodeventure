701fc5d65cc1ad7e0a7f348d6e4eb1733e7eb695
seraph30 <seraphina.anderson@googlemail.com>
2013-09-26 21:08:39 +0100

    Update garden.js

--- world/garden.js
+++ world/garden.js
@@ -1,7 +1,7 @@
 room('garden', {
   description: "The garden is overgrown with weeds, to the point where you can't see what's growing in it at all! There is a hoe nearby- use it. ",
   image: "http://cabinetmagazine.org/issues/2/beige.jpg",
-  exits: { east: 'home', backalong: 'soup kitchen'},
+  exits: { east: 'home', backalong: 'soup kitchen' front: 'spiderweb' },
   items: [
     {
       name: 'threeheads',
