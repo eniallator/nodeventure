defc530a34ce4c9fd5f0b3349921228e13f12769
thHove <tim.hewson@NTLworld.com>
2013-09-26 20:03:39 +0100

    Update hove-w.js

--- world/hove-w.js
+++ world/hove-w.js
@@ -1,7 +1,7 @@
 room('hove_w', {
   description: "further along the beach. North of westbeach.",
   //image: "http://farm9.staticflickr.com/8116/8661273072_50e771036e.jpg",
-  exits: { north:'westbeach' , east:'the_uncreated_room'  },
+  exits: { north:'westbeach' , east:'the_uncreated_room', south:'bellerbyscollege'},
   items: [
     {
       name: 'coffee shop',
