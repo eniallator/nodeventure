174c8c11b603d7175f5d763d36a634b0933c9da8
seraph30 <seraphina.anderson@googlemail.com>
2013-09-26 20:42:44 +0100

    A very overgrown garden
    Weeds roam here!

--- world/garden.js
+++ world/garden.js
@@ -1,4 +1,3 @@
-
 room('garden', {
   description: "The garden is overgrown with weeds, to the point where you can't see what's growing in it at all! There is a hoe nearby- use it. ",
   image: "http://cabinetmagazine.org/issues/2/beige.jpg",
