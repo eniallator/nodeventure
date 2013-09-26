dbc5aeaa4ba3cef3d97d9628e19038432728abe5
Steve Thomson <organicpanda@gmail.com>
2013-09-26 22:04:34 +0100

    Giving oldlady a story

--- world/oldlady.js
+++ world/oldlady.js
@@ -1,9 +1,23 @@
+var startChatToOldlady = function(player, game) {
+
+  oldlady.execute('say Hello dearie, I\'m so upset because my daughter here has died.');
+  player.write('The Old Lady shows you a beaten up looking doll with a missing eye.');
+  oldlady.execute('say I wonder if you can find me a glass eye and some thread so I can put her eye back. I think we have a chance to bring her back to life.');
+
+  return {
+    'yes' : ['Yes I will help', function(player, game) { player.write('Thank you'); }],
+    'no' : ['Sorry but no', function(player, game) { player.write('Boo'); }]
+  };
+
+}
+
 var oldlady = character('oldlady', {
   location: 'dollhouse',
   description: 'An old lady. She seems strangely interested in one of the dolls in particular.',
   onReceive: function(player, item) {
     player.write(item.name);
-  }
+  },
+  talk: startChatToOldlady
 });
 
 handler('tick', function () {
@@ -12,4 +26,3 @@ handler('tick', function () {
     oldlady.execute('say Oh my poor poor dear');
   }  
 });
-
