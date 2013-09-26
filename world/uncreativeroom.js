cfb9bb8f4acbdb70cfc3794e4fb897c835c13a8b
happytom <ilchenko.daniil280@gmail.com>
2013-09-26 20:20:28 +0100

    Update uncreativeroom.js

--- world/uncreativeroom.js
+++ world/uncreativeroom.js
@@ -1,7 +1,7 @@
 room('the_uncreative_room', {
   description: "A bleak dark room, void of anything withholding or substantial. All that can be made out is a lone object sitting in the only pool of light. Being here creates a feeling that imagination and creativity is sapped away moment by moment.",
   image: "http://www.koreanbeacon.com/wp-content/uploads/2011/06/20-lee_ufan_naoshima_museum_silence_room.jpg",
-  exits: { south: 'westbeach', west 'hove_W'  }, 
+  exits: { south: 'westbeach', west: 'hove_W', east: 'spinningcube'  }, 
   items: [
     {
       name: 'An uncoloured shapeless mass',
