46009549120e1bce46ceeb97fd7e909f4efe7782
Andy Baker <andy@andybak.net>
2013-09-26 20:48:27 +0100

    Send 'em to hell

--- world/pull.js
+++ world/pull.js
@@ -17,9 +17,11 @@ function (rest, player, game) {
     return;
   }
 
-  var rooms = game.rooms;
-  var names = _.keys(rooms).join(" ")
-  player.write(names);
+  player.write("You hear a deafening wooshing sound as if a million elephants all gasped in unison. Polychromatic snowflakes descend and cover your body as you dissolve into everything all at once.");
+  player.setCurrentRoom('the_abyss');
+  //var rooms = game.rooms;
+  //var names = _.keys(rooms).join(" ")
+  //player.write(names);
 
 });
 
